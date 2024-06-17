0561bf03aac7686f895198385934575d28d16018
Haldun Anil <haldunanil@gmail.com>
2024-06-17 18:01:39 -0400

    ci: replace commitlint with changesets

--- commitlint.config.js
+++
@@ -1,7 +0,0 @@
-/** @type {import("@commitlint/core").Config} */
-export default {
-  extends: ["@commitlint/config-conventional"],
-  rules: {
-    "header-max-length": [2, "always", 280],
-  },
-};
