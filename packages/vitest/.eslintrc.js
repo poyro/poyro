588ed1086145f1c656b9485f470f5e507fd915e6
Haldun Anil <haldunanil@gmail.com>
2024-06-17 17:21:11 -0400

    chore: fix eslint configs

--- packages/vitest/.eslintrc.js
+++ packages/vitest/.eslintrc.js
@@ -1,5 +1,6 @@
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
+  extends: ["@repo/eslint-config/server.js"],
   parser: "@typescript-eslint/parser",
   parserOptions: {
     project: true,
