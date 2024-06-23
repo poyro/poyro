b15f3dc909f38b826e93649c81ddb62815f5b4e9
Haldun Anil <haldunanil@gmail.com>
2024-06-23 17:28:01 -0400

    perf: use context caching to improve performance and switch to Llama-3

---
+++ packages/vitest/src/makeModel.ts
@@ -0,0 +1,94 @@
+import path from "node:path";
+
+import type {
+  GbnfJsonSchema,
+  LLamaChatPromptOptions,
+  Llama,
+  LlamaContextSequence,
+  LlamaModel,
+} from "node-llama-cpp";
+import {
+  LlamaChatSession,
+  getLlama,
+  LlamaJsonSchemaGrammar,
+} from "node-llama-cpp";
+
+import { getModelPath } from "./utils/getModelPath";
+
+export interface SessionOptions {
+  /** The system prompt to use */
+  systemPrompt?: string;
+}
+
+class PoyroModelCore {
+  private llama: Llama;
+  private model: LlamaModel;
+  private contextSequence: LlamaContextSequence | undefined;
+
+  constructor(llama: Llama, model: LlamaModel) {
+    this.llama = llama;
+    this.model = model;
+  }
+
+  private async _getContextSequence(): Promise<LlamaContextSequence> {
+    // If there is already a context sequence in memory, return it
+    if (this.contextSequence) {
+      return this.contextSequence;
+    }
+
+    // Otherwise, create a new context
+    const context = await this.model.createContext();
+
+    // Then get the sequence
+    this.contextSequence = context.getSequence();
+
+    return this.contextSequence;
+  }
+
+  private async _createSession({
+    systemPrompt,
+  }: SessionOptions): Promise<LlamaChatSession> {
+    const contextSequence = await this._getContextSequence();
+
+    return new LlamaChatSession({
+      contextSequence,
+      systemPrompt,
+      autoDisposeSequence: false,
+    });
+  }
+
+  public createGrammar<const T extends Readonly<GbnfJsonSchema>>(
+    grammar: T
+  ): LlamaJsonSchemaGrammar<T> {
+    return new LlamaJsonSchemaGrammar(this.llama, grammar);
+  }
+
+  public async prompt(
+    prompt: string,
+    { systemPrompt, ...options }: LLamaChatPromptOptions & SessionOptions = {}
+  ): Promise<string> {
+    // Create a new session
+    const session = await this._createSession({
+      systemPrompt,
+    });
+
+    // prompt the session
+    const answer = await session.prompt(prompt, options);
+
+    return answer;
+  }
+}
+
+export const makeModel = async (): Promise<PoyroModelCore> => {
+  // Get the directory path and filename for the model
+  const { dirPath, filename } = getModelPath();
+
+  // Put together the file path
+  const modelPath = path.join(dirPath, filename);
+
+  // Create a new llama model
+  const llama = await getLlama();
+  const model = await llama.loadModel({ modelPath });
+
+  return new PoyroModelCore(llama, model);
+};
--- packages/vitest/src/matchers/toFulfillCriterion/template.md
+++ packages/vitest/src/matchers/toFulfillCriterion/template.md
@@ -1,15 +1,3 @@
-You are a fair judge assistant tasked with providing clear, objective feedback based on a specific criterion, ensuring each assessment reflects the absolute standards set for performance.
-
-You will be given a response to evaluate, a binary criterion to evaluate against, and an optional instruction (might include an Input inside it). You must provide feedback based on the given criterion and the response.
-
-Please follow these guidelines:
-
-1. Write a detailed feedback that assess the quality of the response strictly based on the given score rubric, not evaluating in general.
-2. After writing feedback, write a determination True or False about whether the response meets the binary criteria specified.
-3. Do not generate any other opening, closing, and explanations.
-4. Keep your feedback concise and clear, do not repeat yourself and do not exceed 280 characters for the feedback.
-5. Only describe the boolean result as True/False, do not use any other words to describe the result.
-
 {{#if additionalContext}}
 
 ### The instruction to evaluate:
