4976fe68a1536b56faaa7c3da12813cb8cbd9f64
Haldun Anil <haldunanil@gmail.com>
2024-06-26 00:09:00 -0400

    ci: only prevent deployment when CHANGELOG isn't changed on the main branch

--- packages/docs/scripts/check_changelog_changes.sh
+++ packages/docs/scripts/check_changelog_changes.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Get the current branch name
+current_branch=$(git rev-parse --abbrev-ref HEAD)
+
+# Check if the current branch is 'master'
+if [ "$current_branch" != "master" ]; then
+  echo "✅ - Not on 'main' branch. Skipping CHANGELOG check. Deploying the documentation..."
+  exit 1;
+fi
+
 # Get the list of changed files between the last commit and the current commit
 changed_files=$(git diff --name-only HEAD HEAD~1)
 
