abdc53f822e80023892b1a249026be064c8b94d1
Haldun Anil <haldunanil@gmail.com>
2024-06-25 23:46:51 -0400

    ci: only deploy the docs site when the CHANGELOG.md file has changed

---
+++ packages/docs/scripts/check_changelog_changes.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Get the list of changed files between the last commit and the current commit
+changed_files=$(git diff --name-only HEAD HEAD~1)
+
+# Check if any of the changed files is a CHANGELOG.md
+if echo "$changed_files" | grep -q "packages/docs/CHANGELOG.md"; then
+  # Proceed with the build if any CHANGELOG.md file has changed
+  echo "✅ - Docs CHANGELOG.md file has changed. Deploying the documentation..."
+  exit 1;
+else
+  echo "🛑 - No changes in docs CHANGELOG.md file. Cancelling the deployment of the documentation..."
+  exit 0;
+fi
