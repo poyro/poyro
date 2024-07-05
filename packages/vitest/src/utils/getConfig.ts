046d95153826093d6c4f0683994f1af454f12f06
Haldun Anil <haldunanil@gmail.com>
2024-07-05 14:58:18 -0400

    feat: allow users to configure poyro behavior via 'poyro.config.js'

---
+++ packages/vitest/src/utils/getConfig.ts
@@ -0,0 +1,40 @@
+import fs from "node:fs";
+import path from "node:path";
+
+import { assign } from "radash";
+
+import type { PoyroVitestConfig } from "../config";
+import { defaultConfig } from "../config";
+
+import { validateConfig } from "./validateConfig";
+
+export const getConfig = async (): Promise<PoyroVitestConfig> => {
+  // Get current working dir
+  const cwd = process.cwd();
+
+  // Get the config file path
+  const configPath = path.resolve(cwd, "poyro.config.js");
+
+  // If the file does not exist, return the default config
+  if (!fs.existsSync(configPath)) {
+    return validateConfig(defaultConfig);
+  }
+
+  // Otherwise, require the config file
+  const config = (await import(configPath).then(
+    (m) => m.default
+  )) as PoyroVitestConfig;
+
+  // If the config is not an object, throw an error
+  if (typeof config !== "object") {
+    throw new Error("The config must be an object.");
+  }
+
+  // If the config file is not valid, throw an error
+  if (!config) {
+    throw new Error("Something went wrong while loading the config file.");
+  }
+
+  // Return the merged config
+  return validateConfig(assign(defaultConfig, config));
+};
