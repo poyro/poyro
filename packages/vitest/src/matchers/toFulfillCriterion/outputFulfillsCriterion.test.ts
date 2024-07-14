7b1c24abf80d0b4dbca58389819f38014b0cdc35
dabs9 <dabulhosa@gmail.com>
2024-07-14 16:13:50 -0700

    refactor: make tests truly sequential

--- packages/vitest/src/matchers/toFulfillCriterion/outputFulfillsCriterion.test.ts
+++ packages/vitest/src/matchers/toFulfillCriterion/outputFulfillsCriterion.test.ts
@@ -32,39 +32,57 @@ describe("outputFulfillsCriterion", () => {
     ["The first manned airplane flight was in 1903, by the Wright brothers.", "Mentions cars"]
   ]
 
+  const allExamples = trueExamples.concat(falseExamples);
+
   it("all true - should pass for all examples", async () => {
-    const results = trueExamples.map(async ([output, criterion]) => {
-      return (await outputFulfillsCriterion(output, criterion)).result;
-    });
-    const allAreTrue = (await Promise.all(results)).every(result => result);
+    const results = [];
+
+    for (const [output, criterion] of trueExamples) {
+      const result = await outputFulfillsCriterion(output, criterion);
+      results.push(result.result);
+    }
+
+    const allAreTrue = results.every(result => result);
 
     expect(allAreTrue).toBe(true);
-  }, 10000)
+  }, 20000)
 
   it("all false - should fail for all examples", async () => {
-    const results = falseExamples.map(async ([output, criterion]) => {
-      return (await outputFulfillsCriterion(output, criterion)).result;
-    });
-    const allAreFalse = (await Promise.all(results)).every(result => !result);
+    const results = [];
+
+    for (const [output, criterion] of falseExamples) {
+      const result = await outputFulfillsCriterion(output, criterion);
+      results.push(result.result);
+    }
+
+    const allAreFalse = results.every(result => !result);
 
     expect(allAreFalse).toBe(true);
-  }, 10000)
+  }, 20000)
   
   it("mixed - should fail for all examples", async () => {
-    const results = trueExamples.concat(falseExamples).map(async ([output, criterion]) => {
-      return (await outputFulfillsCriterion(output, criterion)).result;
-    });
-    const someAreFalse = (await Promise.all(results)).every(result => result);
+    const results = [];
 
-    expect(someAreFalse).toBe(false);
-  }, 10000)
+    for (const [output, criterion] of allExamples) {
+      const result = await outputFulfillsCriterion(output, criterion);
+      results.push(result.result);
+    }
+
+    const allAreTrue = results.every(result => result);
+
+    expect(allAreTrue).toBe(false);
+  }, 20000)
 
   it("mixed - should pass for at least one examples", async () => {
-    const results = trueExamples.concat(falseExamples).map(async ([output, criterion]) => {
-      return (await outputFulfillsCriterion(output, criterion)).result;
-    });
-    const someAreTrue = (await Promise.all(results)).some(result => result);
+    const results = [];
+
+    for (const [output, criterion] of allExamples) {
+      const result = await outputFulfillsCriterion(output, criterion);
+      results.push(result.result);
+    }
+
+    const someAreTrue = results.some(result => result);
 
     expect(someAreTrue).toBe(true);
-  }, 10000)
+  }, 20000)
 });
