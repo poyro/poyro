ec951a6bf1b3f601a8a8c01461b444181f8cc295
Haldun Anil <haldunanil@gmail.com>
2024-07-14 16:13:38 -0400

    feat: add new entrypoint for functions at '@poyro/vitest/functions'

--- packages/vitest/tsup.config.ts
+++ packages/vitest/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig, type Options } from "tsup";
 
 export default defineConfig((options: Options) => ({
-  entryPoints: ["src/index.ts", "src/config.ts"],
+  entryPoints: ["src/index.ts", "src/config.ts", "src/functions.ts"],
   format: ["esm"],
   clean: false,
   dts: true,
