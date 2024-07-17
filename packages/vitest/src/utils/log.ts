3b5da8fbf13a164a73e51d141cbf6d679dd6d96e
Haldun Anil <haldunanil@gmail.com>
2024-07-17 00:45:03 -0400

    use console.log instead of process.stdout in ci

--- packages/vitest/src/utils/log.ts
+++ packages/vitest/src/utils/log.ts
@@ -5,5 +5,10 @@
  * @returns Whether the message was successfully logged
  */
 export const log = (message: string): boolean => {
+  if (process.env.CI === "true") {
+    console.log(message);
+    return true;
+  }
+
   return process.stdout.write(message);
 };
